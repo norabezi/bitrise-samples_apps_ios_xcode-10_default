658f3bd138587b88f127e562d44d07c0899188ff
Akos Birmacher <akos.birmacher@bitrise.io>
2019-01-25 16:40:17 +0100

    Initial Commit

---
+++ Xcode-10_default/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode-10_default
+//
+//  Created by Akos Birmacher on 2019. 01. 25..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Xcode-10_defaultTests/Xcode_10_defaultTests.swift
@@ -0,0 +1,34 @@
+//
+//  Xcode_10_defaultTests.swift
+//  Xcode-10_defaultTests
+//
+//  Created by Akos Birmacher on 2019. 01. 25..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode_10_default
+
+class Xcode_10_defaultTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
